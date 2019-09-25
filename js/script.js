65ff8516e37399cb9c335ae45c10b1617d1642b0
FSDonnelly <donnelly_frank@yahoo.com>
2019-09-25 00:05:48 -0700

    add contact section

--- js/script.js
+++ js/script.js
@@ -34,6 +34,8 @@ $(document).ready(function() {
   });
 
   let skillsTopOffset = $('.skills-section').offset().top;
+  let statsTopOffset = $('.stats-section').offset().top;
+  let countUpFinished = false;
 
   $(window).scroll(function() {
     if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
@@ -51,12 +53,17 @@ $(document).ready(function() {
         }
       });
     }
-  });
+    if (
+      !countUpFinished &&
+      window.pageYOffset > statsTopOffset - $(window).height() + 200
+    ) {
+      $('.counter').each(function() {
+        let element = $(this);
+        let endVal = parseInt(element.text());
 
-  $('.counter').each(function() {
-    let element = $(this);
-    let endVal = parseInt(element.text());
-
-    element.countup(endVal);
+        element.countup(endVal);
+      });
+      countUpFinished = true;
+    }
   });
 });
